cb415eb22bb176d612ab45a2a7440dbfabf34302
varje <varjekivila@gmail.com>
2025-08-11 16:56:58 +0300

    add auth tokens to outgoing requests

--- frontend/src/pages/Authentication.js
+++ frontend/src/pages/Authentication.js
@@ -34,5 +34,11 @@ export async function action({ request }) {
       }
     );
   }
+
+  const resData = await response.json();
+  const token = resData.token;
+
+  localStorage.setItem('token', token);
+
   return redirect('/');
 }
